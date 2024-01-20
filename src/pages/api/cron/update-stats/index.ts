e8bb89a1ad33443704e46b6a9cc976e0c43d845c
wingstako <wingstako@proton.me>
2024-01-19 16:31:11 -0800

    feat(wip): fetch github stat cron job

---
+++ src/pages/api/cron/update-stats/index.ts
@@ -0,0 +1,120 @@
+import type { Repository, RepositoryClones, RepositoryViews, User } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { DevBundler } from 'next/dist/server/lib/router-utils/setup-dev-bundler'
+import { db } from '~/server/db'
+
+type DetailedStat = {
+  timestamp: string
+  count: number
+  uniques: number
+}
+
+type ViewResponse = {
+  count: number
+  uniques: number
+  views: DetailedStat[]
+}
+
+type CloneResponse = {
+  count: number
+  uniques: number
+  clones: DetailedStat[]
+}
+
+type Response = {
+  updatedRepositories: { owner: string, repo: string }[];
+  total: number;
+} | {
+  error: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Response>
+) {
+
+  try {
+    const response: Response = {
+      updatedRepositories: [],
+      total: 0
+    };
+
+    await db.repository.update({
+      where: {
+        id: 1,
+      },
+      data: {
+        name: 'wingstako-neocities',
+      }
+    })
+
+    const repositories = await db.repository.findMany({
+      include: {
+        user: {
+          include: {
+            accounts: true,
+          }
+        },
+        repositoryClones: true,
+        repositoryViews: true,
+      }
+    });
+
+    for (const repository of repositories) {
+
+      const owner = (repository.user as User)?.name ?? '';
+      const repo = repository.name;
+
+      const githubAccessToken = repository.user?.accounts[0]?.access_token ?? null;
+
+      const githubViewsAPI = `https://api.github.com/repos/${owner}/${repo}/traffic/views`;
+      const githubClonesAPI = `https://api.github.com/repos/${owner}/${repo}/traffic/clones`;
+
+      // fetch views
+      const viewsResponse = await fetch(githubViewsAPI, {
+        headers: {
+          Authorization: `token ${githubAccessToken}`
+        }
+      });
+
+      const viewsData: ViewResponse = await viewsResponse.json() as ViewResponse;
+      await db.repositoryViews.createMany({
+        data: viewsData.views.map((view) => {
+          return {
+            timestamp: view.timestamp,
+            count: view.count,
+            uniques: view.uniques,
+            repositoryId: repository.id,
+          }
+        })
+      });
+
+      // fetch clones
+      const clonesResponse = await fetch(githubClonesAPI, {
+        headers: {
+          Authorization: `token ${githubAccessToken}`
+        }
+      });
+
+      const clonesData: CloneResponse = await clonesResponse.json() as CloneResponse;
+      await db.repositoryClones.createMany({
+        data: clonesData.clones.map((clone) => {
+          return {
+            timestamp: clone.timestamp,
+            count: clone.count,
+            uniques: clone.uniques,
+            repositoryId: repository.id,
+          }
+        })
+      });
+
+      response.updatedRepositories.push({ owner, repo });
+      response.total++;
+    }
+
+    return res.status(200).json(response);
+  }
+  catch (error) {
+    return res.status(500).json({ error: JSON.stringify(error) });
+  }
+}
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -10,12 +10,6 @@ import GitHubProvider from "next-auth/providers/github";
 import { env } from "~/env";
 import { db } from "~/server/db";
 
-/**
- * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
- * object and keep type safety.
- *
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- */
 declare module "next-auth" {
   interface Session extends DefaultSession {
     user: DefaultSession["user"] & {
@@ -31,18 +25,16 @@ declare module "next-auth" {
   // }
 }
 
-/**
- * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
- * @see https://next-auth.js.org/configuration/options
- */
 export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: 'jwt'
+  },
   callbacks: {
-    session: ({ session, user }) => ({
+    session: ({ session, token }) => ({
       ...session,
       user: {
         ...session.user,
-        id: user.id,
+        id: token.sub,
       },
     }),
   },
@@ -55,23 +47,9 @@ export const authOptions: NextAuthOptions = {
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the GITHUB provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
 };
 
-/**
- * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
- *
- * @see https://next-auth.js.org/configuration/nextjs
- */
 export const getServerAuthSession = (ctx: {
   req: GetServerSidePropsContext["req"];
   res: GetServerSidePropsContext["res"];
