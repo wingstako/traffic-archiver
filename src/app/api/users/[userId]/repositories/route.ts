77fab20706fe1cf21e1ed66e185d0c21ed354e6f
wingstako <wingstako@proton.me>
2024-01-24 16:21:25 -0800

    fix: add dummy response

--- src/app/api/users/[userId]/repositories/route.ts
+++ src/app/api/users/[userId]/repositories/route.ts
@@ -1,4 +1,4 @@
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function GET(
   request: NextRequest,
@@ -7,9 +7,5 @@ export async function GET(
   const userId = params.userId;
   const provider = request.nextUrl.searchParams.get("provider") ?? "default";
 
-  if (provider === "github") {
-    return await import("~/app/api/users/[userId]/repositories/github/route").then(
-      (module) => module.GET(request, { params })
-    );
-  }
+  return NextResponse.json({}, {status: 200});
 }
